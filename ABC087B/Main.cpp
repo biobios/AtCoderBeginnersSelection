d89b5848cbbb3f8e38de905c0a3378fa2c22b890
biobios <teiketsuatsu.lbp@gmail.com>
2024-06-27 01:32:35 +0900

    clear: ABC087B

--- ABC087B/Main.cpp
+++ ABC087B/Main.cpp
@@ -6,42 +6,75 @@ int main() {
 	int A, B, C, X;
 	cin >> A >> B >> C >> X;
 
+
+
 	unsigned int result = 0;
 	unsigned int count500, count100, count50;
 
 	count500 = X / 500;
-	count100 = (X % 500) / 100;
-	count50 = (X % 100) / 50;
+	
+	int64_t use500, use100, use50;
 
-	size_t i;
-	if (count500 > A) {
-		i = count500 - A;
+	if (count500 >= A) {
+		use500 = A;
 	}
 	else {
-		i = 0;
+		use500 = count500;
 	}
 
-	for (; i <= count500; i++) {
-		unsigned int count500_dash = count500 - i;
-		unsigned int count100_dash = count100 + 5 * i;
-
-		size_t j;
-		if (count100_dash > B) {
-			j = count100_dash - B;
+	for (; use500 >= 0; use500--) {
+		count100 = ((X % 500) + 500 * (count500 - use500)) / 100;
+		if (count100 >= B) {
+			use100 = B;
 		}
 		else {
-			j = 0;
+			use100 = count100;
 		}
 
-		for (; j <= count100_dash; j++) {
-			unsigned int count100_dash_dash = count100_dash - j;
-			unsigned int count50_dash = count50 + 2 * j;
+		for (; use100 >= 0; use100--) {
+			count50 = ((X % 100) + 100 * (count100 - use100)) / 50;
+			
+			if (count50 > C)
+				break;
 
-			if (count50_dash > C)break;
-			if (count100_dash_dash <= B && count500_dash <= A) {
-				result++;
-			}
+			result++;
+
 		}
 	}
+
+	cout << result << endl;
+
+	return 0;
+
+	//size_t i;
+	//if (count500 > A) {
+	//	i = count500 - A;
+	//}
+	//else {
+	//	i = 0;
+	//}
+
+	//for (; i <= count500; i++) {
+	//	unsigned int count500_dash = count500 - i;
+	//	unsigned int count100_dash = count100 + 5 * i;
+
+	//	size_t j;
+	//	if (count100_dash > B) {
+	//		j = count100_dash - B;
+	//	}
+	//	else {
+	//		j = 0;
+	//	}
+
+	//	for (; j <= count100_dash; j++) {
+	//		unsigned int count100_dash_dash = count100_dash - j;
+	//		unsigned int count50_dash = count50 + 2 * j;
+
+	//		if (count50_dash > C)break;
+	//		if (count100_dash_dash <= B && count500_dash <= A) {
+	//			result++;
+	//		}
+	//	}
+	//}
 
 }
